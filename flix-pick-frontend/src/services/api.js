ff0e7189c910547f9019b55fde25fe9d4b6c7b44
Dyuti Mengji <73112092+DyutiM25@users.noreply.github.com>
2025-03-01 13:34:26 -0700

    Update api.js

--- flix-pick-frontend/src/services/api.js
+++ flix-pick-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 //it is usually a good practice to create a separate file that contains all of your api calls so that you can keep all the networking operations or stuff related to the API in a separate file and find it easily.
 //encodeURIComponent() => removes anything from the string that we cannot pass in the URL
-const API_KEY = "eb96676b84420817a707ec57b15b11c0";
+const API_KEY = "";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
